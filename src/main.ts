93a34fccc470f0bcf94d8c81e250464fa6855dc7
marcelo.kade <marcelo.kade@gmail.com>
2026-09-15 19:15:24 -0300

    config: enable cors and change api port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,7 +17,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 bootstrap();
